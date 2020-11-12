d88e7a8f961b115ea8f7b9b0b577603af4d993d3
Elpichula404 <rafanachomendez@gmail.com>
2020-11-11 22:39:14 -0300

    modificacion estrella señores
    soy añañin

--- WebTelefonos/admin.py
+++ WebTelefonos/admin.py
@@ -2,8 +2,5 @@ from django.contrib import admin
 from .models import Marca, Producto,Contacto
 
 admin.site.register(Contacto)
-
 admin.site.register(Marca)
-def (claudiowecoxd):
- 
 admin.site.register(Producto)
